f84a15668edf75d11b7c7a603f387392beaa68ee
voviz <zaharov.va@edu.spbstu.ru>
2020-11-04 18:40:24 +0300

    Всем привет я Вова

--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -238,5 +238,6 @@ class Tests {
         assertEquals("двести тысяч два", russian(200002))
         assertEquals("девятьсот тысяч", russian(900000))
         assertEquals("двенадцать", russian(12))
+        assertEquals("двенадцать тысяч сто двадцать три", russian(12123))
     }
 }
